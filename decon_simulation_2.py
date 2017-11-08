dd0b306f0ea1d57f25e1cc9c7b578cce09483a9a
Christa.Caggiano <Christa.Caggiano@UCSFs-MacBook-Pro.local>
2017-11-08 09:31:56 -0800

    updated model

--- decon_simulation_2.py
+++ decon_simulation_2.py
@@ -12,38 +12,55 @@ import matplotlib.cm as cm
 def generate_proportion(tissue, individuals):
     rows = []
     for i in range(individuals):
-        vals = np.random.choice(100, tissue)
+        # half_time = random.randint(0, 1)
+        # if half_time == 1:
+
+        vals = np.random.choice(10, tissue)
         rows.append([x/sum(vals) for x in vals])
     return pd.DataFrame.from_records(rows)
 
 
-def generate_reference(tissue, sites, mu_max, sigma):
-    a, b = random.sample(range(1, 5), 2)
+def generate_reference(tissue, sites):
     rows = []
     for t in range(tissue):
-        vals = np.random.beta(a, b, sites)
-        rows.append([x for x in vals])
-    return pd.DataFrame.from_records(rows)
+        vals = []
+        for s in range(sites):
+            vals.append(random.randint(0,1))
+            # half_time = random.randint(0, 1)
+            # if half_time == 1:
+            #     vals.append(random.randint(0, 1))
+            # else:
+            #     a = 0.5
+            #     b = 0.5
+            #     vals.append(np.random.beta(a, b))
+        rows.append(vals)
+    return pd.DataFrame(rows).round(decimals=2)
 
 
 if __name__ == "__main__":
 
     individuals = 5
-    sites = 10
+    sites = 1000
     tissues = 10
 
-    mu_max = 0.5
-    sigma = 1
-
     p = generate_proportion(tissues, individuals).as_matrix()
-    r = generate_reference(tissues, sites, mu_max, sigma).as_matrix()
-    o = np.dot(p, r)
+    r = generate_reference(tissues, sites).as_matrix()
+    o = np.around(np.dot(p, r), decimals=2)
 
-    # print(o)
-    # colors = cm.rainbow(np.linspace(0, 1, len(o)))
-    # for i, c in zip(range(len(o)), colors):
-    #     plt.scatter(range(len(o[i, :])), o[i, :], color=c, label="individual: " + str(i))
-    # plt.xlabel("CpG site")
-    # plt.ylabel("Proportion methylated")
-    # # plt.legend(loc=0)
-    # plt.show()
+    x, y = o.shape
+    for i in range(x):
+        for j in range(y):
+            if random.randrange(0, 1) == 1:
+                o[i][j] = random.choice([0, 1])
+            if random.randrange(0, 10) == 1:
+                o[i][j] = 0.5
+            if random.randrange(0, 10) == 1:
+                o[i][j] = 0.75
+
+    colors = cm.rainbow(np.linspace(0, 1, len(o)))
+    for i, c in zip(range(len(o)), colors):
+        plt.scatter(range(len(o[i, :])), o[i, :], color=c, label="individual: " + str(i))
+    plt.xlabel("CpG site")
+    plt.ylabel("Proportion methylated")
+    # plt.legend(loc=0)
+    plt.show()
