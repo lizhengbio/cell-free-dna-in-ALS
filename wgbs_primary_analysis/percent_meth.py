2da57c9a5c0ef9b3c920e0a19da7c8a1a6bf46ca
christacaggiano <christacaggiano@gmail.com>
2017-12-21 21:52:58 -0800

    actual version i ran

--- wgbs_primary_analysis/percent_meth.py
+++ wgbs_primary_analysis/percent_meth.py
@@ -1,45 +1,61 @@
+
 # calculating percent methylated sites
+# author <arya@aryaboudaie.com>
 # author <christa.caggiano@ucsf.edu>
-# author <aryab@google.com>
 from __future__ import print_function, division
 from collections import defaultdict
 import glob
+import argparse
 
  
-# glob.glob("SRR*/unsortedButMerged_ForBismark_file/methylation_extraction/CpG_context_SRR*_unsorted_merged.deduplicated.txt")
-def write_results(key, value, output_file):
-    with open(output_file, "a") as out:
-        print("{} {} {} {} {}".format(key[0], key[1], value[0], value[1], value[0]/(value[0]+value[1])), file=out)
-
 
 def run(input_file, output_file):
     i = 0
-    previous_key = []
-    pluses = 0
-    minuses = 0
+    d = defaultdict(lambda: [0,0])
     with open(input_file) as f:
         for line in f:
             if i == 0:
                 i += 1
                 continue
             line_split = line.split()
-            key = [line_split[2], line_split[3]]
-            if previous_key and key != previous_key:
-                write_results(previous_key, [pluses, minuses], output_file)
-                pluses = 0
-                minuses = 0
-
-            previous_key = key
+            key = line_split[2]+"|"+line_split[3]
 
             sign = line_split[1]
             if sign == "+":
-                pluses += 1
+                d[key][0]+=1
             else:
-                minuses += 1
+                d[key][1] += 1
             i += 1
+    with open(output_file, "w") as out:
+        for key, value in d.items():
+            chrom, site = key.split("|")
+            print("{} {} {} {} {}".format(chrom, site, value[0], value[1], value[0] + value[1], value[0]/(value[0]+value[1])*100, "+"), file=out)
+
 
 
 if __name__ == "__main__":
-    filenames = glob.glob("SRR*/unsortedButMerged_ForBismark_file/methylation_extraction/CpG_context_SRR*_unsorted_merged.deduplicated.txt")
-    for fn in filenames:
-        run(fn, fn.split("/CpG_context_SRR")[0]+"/results.txt")
+
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("file_name", type=int, help="fastq file for processing")
+    args = parser.parse_args()
+    file_name = args.file_name
+    file_name = file_name - 1 
+    
+    f = open("prefixes.txt", "r")
+
+    file_list = []
+    i = 0
+    name = ""
+    for line in f: 
+        if i<file_name:
+            i+=1
+        else:
+            name = line.rstrip()
+            break
+
+
+    fn = name 
+    run(fn, "results_{}.txt".format(name))
+
