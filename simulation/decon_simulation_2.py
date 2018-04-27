9f66921935630371fa2ccdcd55d1d5b97190d650
Christa.Caggiano <christacaggiano@gmail.com>
2018-04-27 12:20:56 -0700

    updated to correctly minimize with log likelihood

--- simulation/decon_simulation_2.py
+++ simulation/decon_simulation_2.py
@@ -6,11 +6,14 @@
 import pandas as pd
 import random
 import numpy as np
+import scipy
+import scipy.stats as stats
 from matplotlib import pyplot as plt
 import matplotlib.cm as cm
+from scipy.optimize import minimize
+from sklearn.metrics import mean_squared_error
 
-
-def generate_proportion(tissue, individuals):
+def generate_proportion(individuals, tissue):
     """
     generates the contribution of different tissues for
     each individual
@@ -36,39 +39,60 @@ def generate_reference(tissue, sites):
     for t in range(tissue):  # generate for each tissue
         vals = []
         for s in range(sites):
-            vals.append(random.randint(0, 1))
+            vals.append(random.uniform(0, 1))
         rows.append(vals)
-    return pd.DataFrame(rows).round(decimals=2)
+    return pd.DataFrame(rows)
 
-# def generate_mixing_proportion(individuals, sites):
-#     for
 
+def log_likelihood(proportions, observed, reference):
+
+    # proportions = proportions/(proportions.sum(axis=0))
+    b = np.transpose(np.matmul(proportions, reference))
+    sigma = np.var(observed-b)
+    N = len(proportions)
+
+    ll = np.sum(-N/2*np.log(2*np.pi) - N/2 * np.log(sigma) - 1/(2*sigma**2) * (np.sum((observed - b)**2)))
+
+    return ll
+
+
+def log_likelihood2(proportions, observed, reference):
+
+    proportions = proportions/(proportions.sum(axis=0))
+    b = np.transpose(np.matmul(proportions, reference))
+    sigma = np.var(observed-b)
+    N = len(proportions)
+
+    ll = np.sum(-N/2*np.log(2*np.pi) - N/2 * np.log(sigma) - 1/(2*sigma**2) * (np.sum((observed - b)**2)))
+
+    return ll
 
 if __name__ == "__main__":
 
-    individuals = 5
-    sites = 10
-    tissues = 2
+    individuals = 1
+    sites = 10000
+    tissues = 100
+    read_depth = 100
 
-    p = generate_proportion(tissues, individuals).as_matrix()
-    r = generate_reference(tissues, sites).as_matrix()
-    print(p)
-    # o = np.around(np.dot(p, r), decimals=2)
+    # @TODO add noise
+    proportions = generate_proportion(individuals, tissues).as_matrix()
+    reference = generate_reference(tissues, sites).as_matrix()
+    observed = np.matmul(proportions, reference)
 
-    # x, y = o.shape
-    # for i in range(x):
-    #     for j in range(y):
-    #         if random.randrange(0, 1) == 1:
-    #             o[i][j] = random.choice([0, 1])
-    #         if random.randrange(0, 10) == 1:
-    #             o[i][j] = 0.5
-    #         if random.randrange(0, 10) == 1:
-    #             o[i][j] = 0.75
-    #
-    # colors = cm.rainbow(np.linspace(0, 1, len(o)))
-    # for i, c in zip(range(len(o)), colors):
-    #     plt.scatter(range(len(o[i, :])), o[i, :], color=c, label="individual: " + str(i))
-    # plt.xlabel("CpG site")
-    # plt.ylabel("Proportion methylated")
-    # # plt.legend(loc=0)
-    # plt.show()
+    proportions_est = np.random.rand(individuals, tissues)
+
+    bounds = tuple((0, 1) for x in range(np.shape(proportions_est)[1]))
+    cons = ({'type': 'eq', 'fun': lambda x: 1 - sum(x)})
+
+    prop_guess_1 = minimize(log_likelihood, proportions_est, args=(observed, reference),
+                          bounds=bounds, constraints=cons, method="SLSQP", options={'maxiter':1000})
+
+    prop_guess_2 = minimize(log_likelihood2, proportions_est, args=(observed, reference),
+                          bounds=bounds, method="L-BFGS-B", options={'maxiter': 1000})
+    # print(prop_guess)
+    # print(prop_guess["x"]/np.sum(prop_guess["x"]))
+    # print(proportions)
+
+    # @TODO mean squared error? what kind of error? make some plots
+    print(mean_squared_error(np.transpose(proportions[0]), (prop_guess_1["x"]/np.sum(prop_guess_1["x"]))))
+    print(mean_squared_error(np.transpose(proportions[0]), (prop_guess_2["x"]/np.sum(prop_guess_2["x"]))))
