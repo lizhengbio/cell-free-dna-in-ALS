029fc904d86635e8989c69852973dd2b663c1278
Christa.Caggiano <christacaggiano@gmail.com>
2018-07-16 13:27:50 -0700

    for one iteration of simulation

--- simulation/run.py
+++ simulation/run.py
@@ -1,4 +1,6 @@
 from optimization.quadratic_programming_optimization import perform_optimization as qp
+from optimization.naive_optimization import perform_optimization as naive
+
 from utils.run_utils import *
 
 
@@ -6,13 +8,16 @@ def generate_optimization(reference, methylated, unmethylated, method):
 
     proportions_est = np.zeros((1, reference.shape[0])) + 0.5  # start with random estimation of proportions
     proportions_est = proportions_est / (np.sum(proportions_est))
+    # print(proportions_est.shape)
     return method(proportions_est, reference, methylated, unmethylated)
 
 
 if __name__ == "__main__":
 
 
-    for patient in ["ctrl1_common_header.txt", "als1_common_header.txt"]:
-        reference, methylated, unmethylated = generate_matrices("/Users/Christa.Caggiano/Desktop/zaitlen_lab_desktop/" + patient, "tissues.txt")
+    for patient in ["deconv_exp1_mix3.txt"]:
+        reference, methylated, unmethylated = generate_matrices("/Users/Christa.Caggiano/Desktop/zaitlen_lab_desktop/" + patient, "data/exp_tissues.txt")
+        np.savetxt("reference.txt", reference)
         x = generate_optimization(reference, methylated, unmethylated, qp)
-        np.savetxt(patient + "_qp__no_WGBS_results.txt", x)
+        print(x)
+        np.savetxt("output/" + patient + "_qp", x)
--- simulation/utils/run_utils.py
+++ simulation/utils/run_utils.py
@@ -5,23 +5,30 @@ import numpy as np
 def generate_matrices(intersect_file, tissue_file):
     data = pd.read_table(intersect_file, low_memory=False)
 
-    reference = data.drop(["chr", "start", "end", "chr.1", "start.1", "end.1",
-                           "total", "chr.2", "start.2", "end.2", "!Sample_geo_accession"], axis=1)
+    reference = data
 
-    reference = reference.replace(to_replace=".", value=np.nan)
+    reference = reference.replace(to_replace="NA", value=np.nan)
     reference = reference.fillna(data.mean())
-    reference = reference.dropna()
 
-    meth_array = reference["meth"].values
-    unmeth_array = reference["unmeth"].values
+    # reference = reference.dropna()
+
+    meth_array = np.round(reference["meth"].values, 0)
+    unmeth_array = np.round(reference["unmeth"].values, 0)
 
     reference = reference.drop(["meth", "unmeth"], axis=1)
 
     tissues = read_tissues(tissue_file)
     average_reference = calculate_tissue_means(reference, tissues)
-
     return average_reference.T, np.reshape(meth_array, (1, len( meth_array))), np.reshape(unmeth_array, (1, len(unmeth_array)))
 
+
+def fill_mode(df):
+    for column in df:
+        # print(df[column].mode()[0])
+        mode = df[column].mode()[0]
+        df[column].fillna(mode, inplace=True)
+
+    return df
 
 def read_tissues(tissue_file):
     tissues = {}
